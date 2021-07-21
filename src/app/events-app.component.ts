2a8a33bea6cb8369a9e1405b20492b704072cdc4
Trend20 <enockomondi305@gmail.com>
2021-07-21 11:15:14 +0300

    temp updates

---
+++ src/app/events-app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'events-app',
+  template: `<h2>Hello World! I am Learning Angular.</h2>
+    <img src="/assets/images/basic-shield.png" />`,
+})
+export class EventsAppComponent {
+  title = 'ng-fundamentals';
+}
