0c67da75bd71b21cf929ae7f7806f12a7db0183a
Trend20 <enockomondi305@gmail.com>
2021-07-21 12:15:31 +0300

    adding list component

---
+++ src/app/events/event-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'events-list',
+  template: `
+    <div>
+      <h1>This is an event list component</h1>
+    </div>
+  `,
+})
+export class EventsListComponent {}
